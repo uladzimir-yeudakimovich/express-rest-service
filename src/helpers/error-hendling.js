d7752c5db46b7029d3c6c9775441293c1249699b
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-04-09 14:48:03 +0300

    added error-handling.js

---
+++ src/helpers/error-hendling.js
@@ -0,0 +1,35 @@
+const {
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+  getStatusText
+} = require('http-status-codes');
+
+class Error {
+  constructor(status) {
+    this.status = status;
+    this.text = getStatusText(this.status);
+  }
+}
+
+function responseToClient(method, res, validReq) {
+  method
+    .then(user => {
+      if (!user) {
+        const err = new Error(NOT_FOUND);
+        throw err;
+      }
+      if (Array.isArray(user)) {
+        res.json(user.map(validReq.toResponse));
+      } else {
+        res.json(validReq.toResponse(user));
+      }
+    })
+    .catch(err => {
+      if (!err.status) {
+        err = new Error(INTERNAL_SERVER_ERROR);
+      }
+      res.status(err.status).send(err.text);
+    });
+}
+
+module.exports = { responseToClient };
