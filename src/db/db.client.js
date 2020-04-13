d409cc4834894b58f86cc50bf87ea528d454cfcb
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-04-14 01:27:21 +0300

    added users to db

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -4,6 +4,7 @@ const { MONGO_CONNECTION_STRING } = require('../common/config');
 const allBoards = require('./boards');
 const allTasks = require('./tasks');
 const allUsers = require('./users');
+const User = require('../resources/users/user.model');
 
 const connectToDb = cb => {
   mongoose.connect(MONGO_CONNECTION_STRING, {
@@ -14,7 +15,9 @@ const connectToDb = cb => {
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', () => {
-    console.log("We're connected");
+    console.log('Connected to DB');
+    db.dropDatabase();
+    User.insertMany(allUsers);
     cb();
   });
 };
--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,17 +1,33 @@
 const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-class User {
-  constructor({ id = uuid(), name, login, password } = {}) {
-    this.id = id;
-    this.name = name;
-    this.login = login;
-    this.password = password;
-  }
+// class User {
+//   constructor({ id = uuid(), name, login, password } = {}) {
+//     this.id = id;
+//     this.name = name;
+//     this.login = login;
+//     this.password = password;
+//   }
 
-  static toResponse(user) {
-    const { id, name, login } = user;
-    return { id, name, login };
-  }
-}
+//   static toResponse(user) {
+//     const { id, name, login } = user;
+//     return { id, name, login };
+//   }
+// }
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: uuid
+    },
+    name: String,
+    login: String,
+    password: String
+  },
+  { versionKey: false }
+);
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
