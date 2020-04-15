fa950ce8e15ba7a2026e5bd7d070bcf8a29bae3f
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-04-16 00:35:01 +0300

    drop data files

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
 
 const { MONGO_CONNECTION_STRING } = require('../common/config');
-const allBoards = require('./boards');
-const allTasks = require('./tasks');
-const allUsers = require('./users');
-const User = require('../resources/users/user.model');
-const Board = require('../resources/boards/board.model');
-const Task = require('../resources/tasks/task.model');
 
-const connectToDb = cb => {
+const connectToDb = callback => {
   mongoose.connect(MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,14 +10,10 @@ const connectToDb = cb => {
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', async () => {
+  db.once('open', () => {
     console.log('Connected to DB');
-    await db.dropDatabase();
-    User.insertMany(allUsers);
-    Board.insertMany(allBoards);
-    Task.insertMany(allTasks);
-    cb();
+    callback();
   });
 };
 
-module.exports = { allBoards, allTasks, allUsers, connectToDb };
+module.exports = { connectToDb };
--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -4,12 +4,15 @@ const getAll = async () => User.find({});
 
 const getUser = async id => User.findById(id);
 
-const addUser = async user => User.create(user);
+const addUser = async user => {
+  if (!user.login || !user.password) return 400;
+  return User.create(user);
+};
 
 const updateUser = async (id, user) => {
+  if (!user.login || !user.password) return 400;
   const userForUpdate = User.find({ _id: id });
   if (!(await userForUpdate).length) return 404;
-  user._id = id;
   await User.findByIdAndUpdate(id, user);
   return User.find({ _id: id });
 };
