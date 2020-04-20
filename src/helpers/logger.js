ac986a73217e8a18b7679ea96fe03df2ba48f129
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-04-20 23:44:14 +0300

    improved logger and added new packages

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -2,32 +2,49 @@ const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
   level: 'silly',
-  format: format.combine(format.colorize(), format.cli()),
   transports: [
-    new transports.Console(),
+    new transports.Console({
+      format: format.combine(format.colorize(), format.cli())
+    }),
     new transports.File({
       filename: 'logs/error.log',
       level: 'error',
-      format: format.combine(format.uncolorize(), format.json())
+      format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:MM:SS' }),
+        format.uncolorize(),
+        format.prettyPrint()
+      )
     }),
     new transports.File({
       filename: 'logs/info.log',
       level: 'info',
-      format: format.combine(format.uncolorize(), format.json())
+      format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:MM:SS' }),
+        format.uncolorize(),
+        format.prettyPrint()
+      )
     })
   ],
   exceptionHandlers: [
     new transports.File({
       filename: 'logs/exceptions.log',
-      format: format.combine(format.uncolorize(), format.json())
+      format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:MM:SS' }),
+        format.uncolorize(),
+        format.prettyPrint()
+      )
     })
   ]
 });
 
-const logRequest = async (req, res, done) => {
+const logRequest = async (req, res, next) => {
   const { url, method, params, body } = req;
-  logger.info(JSON.stringify({ url, method, params, body }));
-  done();
+  logger.info(
+    `url: ${url}, method: ${method}, params: ${JSON.stringify(
+      params
+    )}, body: ${JSON.stringify(body)}`
+  );
+  next();
 };
 
 const logErrors = async (err, req, res, next) => {
