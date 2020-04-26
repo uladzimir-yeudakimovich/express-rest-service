0e2ba130476188ac49b5fdca4d2bbe26b12e1736
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-04-26 10:22:13 +0300

    improved responses

--- src/response/response-status.js
+++ src/response/response-status.js
@@ -3,24 +3,19 @@ const HttpStatus = require('http-status-codes');
 const { logger } = require('../middleware/logger');
 
 const responseStatus = (res, code) => {
-  let text;
-  if (code === 'CONFLICT') {
-    text = 'A user with this login already exists. Use a different login.';
-  } else if (code === 'FORBIDDEN') {
-    text = 'Incorrect login or password';
-  } else if (code === 'NO_CONTENT') {
-    text = 'Has been deleted';
+  const text = {
+    CONFLICT: 'A user with this login already exists. Use a different login.',
+    FORBIDDEN: 'Incorrect login or password',
+    NO_CONTENT: 'Has been deleted'
+  };
+  if (text[code]) {
+    res.status(HttpStatus[code]).send(text[code]);
   } else {
-    text = code;
-  }
-  if (text === code) {
     res
       .status(HttpStatus[code])
       .send(HttpStatus.getStatusText(HttpStatus[code]));
-  } else {
-    res.status(HttpStatus[code]).send(text);
   }
-  logger.info(`status: ${HttpStatus[code]}, text: ${text}`);
+  logger.info(`status: ${HttpStatus[code]}, text: ${text[code] || code}`);
 };
 
 module.exports = responseStatus;
--- src/response/response-to-client.js
+++ src/response/response-to-client.js
@@ -4,17 +4,12 @@ const responseToClient = async (promiss, req, res, model, next) => {
   promiss
     .then(response => {
       if (!response) return responseStatus(res, 'NOT_FOUND');
-      switch (response) {
-        case 204:
-          return responseStatus(res, 'NO_CONTENT');
-        case 409:
-          return responseStatus(res, 'CONFLICT');
-        default:
-          if (Array.isArray(response)) {
-            res.json(response.map(model.toResponse));
-          } else {
-            res.json(model.toResponse(response));
-          }
+      if (response === 204) return responseStatus(res, 'NO_CONTENT');
+      if (response === 409) return responseStatus(res, 'CONFLICT');
+      if (Array.isArray(response)) {
+        res.json(response.map(model.toResponse));
+      } else {
+        res.json(model.toResponse(response));
       }
     })
     .catch(next);
