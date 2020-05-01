0aa7a728c81a3b6bde545ffd85c25bf7a70a7556
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-05-02 02:24:46 +0300

    improved check-token

--- src/middleware/check-token.js
+++ src/middleware/check-token.js
@@ -2,7 +2,6 @@ const { verify } = require('jsonwebtoken');
 
 const { JWT_SECRET_KEY } = require('../common/config.js');
 const responseStatus = require('../response/response-status');
-const usersService = require('../routers/users/user.service');
 
 const checkToken = async (req, res, next) => {
   let token = req.headers.authorization;
@@ -10,10 +9,7 @@ const checkToken = async (req, res, next) => {
   if (token.startsWith('Bearer ')) token = await token.slice(7, token.length);
   verify(token, JWT_SECRET_KEY, async (err, decoded) => {
     if (err) return responseStatus(res, 'UNAUTHORIZED');
-    const user = await usersService.getUser(decoded.id);
-    if (user.login !== decoded.login) {
-      return responseStatus(res, 'UNAUTHORIZED');
-    }
+    req.decoded = decoded;
     next();
   });
 };
